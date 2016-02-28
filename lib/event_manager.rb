0f85348241a5eb0071f1fedeae3b2fab45c4a2ff
Sal Espinosa <sespinos@gmail.com>
2016-02-28 11:49:44 -0700

    Refactor by moving to new method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,18 +8,22 @@ def clean_zipcode(zipcode)
   zipcode = zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-puts "EventManager initialized"
-
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-contents.each do |row|
-  name        = row[:first_name]
-  zipcode     = clean_zipcode(row[:zipcode])
+def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
   legislator_names = legislators.collect do |legislator|
     "#{legislator.first_name} #{legislator.last_name}"
   end
 
-  legislators_string = legislator_names.join(", ")
-  puts "#{name} #{zipcode} #{legislators_string}"
+  legislator_names.join(", ")
+end
+
+puts "EventManager initialized"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name        = row[:first_name]
+  zipcode     = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  puts "#{name} #{zipcode} #{legislators}"
 end
